4d3792604dfbd7914ce1ce4ce58dee378f596f60
suman sinha <sumansinha437@gmail.com>
2025-08-04 09:43:00 +0530

    project setup: frontend (Vite/React/MUI/Tailwind)

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+     <h1 className="text-4xl flex ">hello world</h1>
+    </div>
+  )
+}
+
+export default App
